50ff93e3df51718456e2d2dfdf0a7a25ae4b4ad5
david26044 <davidcarreno767@gmail.com>
2025-11-12 17:15:35 -0500

    Hasta novios

--- src/app/data/memories.ts
+++ src/app/data/memories.ts
@@ -39,7 +39,7 @@ export const MEMORIES: Memory[] = [
   date: '2022-04-25',
   title: 'Nuestra primera salida como algo más que amigos ✨',
   shortText: 'Ese día me sentí muy feliz de tener la oportunidad de compartir contigo.',
-  longText: 'Esta fue nuestra primera foto intentando conocernos. Fue la primera vez que salimos como algo más que amigos, y yo estaba muy feliz de tener la oportunidad de compartir con una muchacha tan linda, con la que sentía una conexión muy especial. Ese día hablamos mucho, nos tomamos fotos, reímos y la pasé increíble. Había algo distinto en el aire, una confianza y una cercanía que no habíamos sentido antes. No sabía en qué se iba a convertir todo esto, pero sí sabía que quería seguir conociéndote. Ese día, sin darme cuenta, empecé a enamorarme de esa sonrisa tan hermosa y de esa niña tan perfecta.',
+  longText: 'Esta fue nuestra primera foto intentando conocernos. Fue la primera vez que salimos como algo más que amigos, y yo estaba muy feliz de tener la oportunidad de compartir con una muchacha tan linda, con la que sentía una conexión muy especial. Ese día hablamos mucho, nos tomamos fotos, reímos y la pasé increíble. Había algo distinto en el aire, una confianza y una cercanía que no habíamos sentido antes. No sabía en qué se iba a convertir todo esto, pero sí sabía que quería seguir conociéndote. Ese día, sin darme cuenta, empecé a enamorarme de esa sonrisa tan hermosa y de esa niña tan perfecta. Fué una etapa muy linda, amé que nos veiamos todos los días, nos gustaba pasar tiempo y nos priorizabamos, siempre sacabamos un momentito para venos.',
   emoji: '✨',
   photo: 'assets/images/primerasalidaalgomas.jpeg'
 },
@@ -48,9 +48,37 @@ export const MEMORIES: Memory[] = [
   date: '2022-05-03',
   title: 'Nuestro primer beso 💋',
   shortText: 'Un momento que nunca voy a olvidar, fue perfecto de principio a fin.',
-  longText: 'Ese día fue muy especial. Hablamos mucho, reímos, nos tomamos fotos y disfrutamos el momento como siempre. Yo estaba nervioso, porque ya habíamos hablado de que queríamos darnos un beso, pero cuando llegó el momento no fui capaz jaja. Al final, cuando íbamos para tu casa, me dijiste que querías dar una vuelta en la moto, y ahí ya sabía por dónde iba esto. Pasamos por un lugar solito que recuerdo perfectamente, me dijiste que parara, te bajaste y me diste nuestro primer beso. Fue un momento espectacular, muy lindo, no pudo ser mejor. Yo pensé que me iba a ir a casa sin ese beso por puro miedo, pero me alegró tanto que al final te lanzaras. Llegué a casa con una sonrisa gigante, feliz y pensando en ti. Cada vez me enamoraba más de esta niña tan hermosa que conocí.',
+  longText: 'Ese día fue muy especial. Hablamos mucho, reímos, nos tomamos fotos y disfrutamos el momento como siempre. Yo estaba nervioso, porque ya habíamos hablado de que queríamos darnos un beso, pero cuando llegó el momento no fui capaz jaja. Al final, cuando íbamos para tu casa, me dijiste que querías dar una vuelta en la moto, y ahí ya sabía por dónde iba esto. Pasamos por un lugar solito que recuerdo perfectamente, me dijiste que parara, te bajaste y me diste nuestro primer beso. Fue un momento espectacular, muy lindo, no pudo ser mejor. Yo pensé que me iba a ir a casa sin ese beso por puro miedo, pero me alegró tanto que al final te lanzaras. Llegué a casa con una sonrisa gigante, feliz y pensando en ti. Cada vez me enamoraba más de esta niña tan hermosa que conocí. Ese día nos volvimos adictos a besaros.',
   emoji: '💋',
   photo: 'assets/images/primerbeso.jpeg'
+},
+{
+  id: 5.5,
+  date: '2022-05-16',
+  title: 'La carta más linda 💌',
+  shortText: 'Un detalle que me llegó al corazón y me hizo sentir muy afortunado.',
+  longText: 'Ese día me diste una carta que jamás voy a olvidar. En ella me expresabas todo lo que sentías, tus ganas de luchar por lo nuestro y de lograrlo todo juntos. Fue un detalle hermoso, sincero, que me llegó directo al corazón. Me hizo muy feliz recibir algo tan especial de la mujer que me estaba enamorando. Recuerdo que llegué tan emocionado a casa que de una le mandé un video a mi hermana para mostrarle lo lindo que había sido. Tus palabras me tocaron de verdad, eran llenas de amor y ternura. En ese momento sentí que el amor que estábamos viviendo era puro y real, de esos que se sienten en el alma. Esa etapa del enamoramiento fue increíblemente bonita, yo era muy feliz, y tú eras y sigues siendo todo lo que siempre quise para mí.',
+  emoji: '💌',
+  photo: 'assets/images/cartalinda.jpeg'
+},
+{
+  id: 6,
+  date: '2022-05-28',
+  title: 'Nuestra primera vez en cine 🍿',
+  shortText: 'Un día especial en Bogotá, donde entendí que quería compartir mi vida contigo.',
+  longText: 'Fue nuestra primera vez en cine y también la primera vez que nos vimos en Bogotá. En ese momento el amor era inmenso, los dos queríamos hacer algo diferente, así que fuimos a ver Doctor Strange. Nos reímos, hablamos, nos tomamos fotos y, claro, también nos besamos mucho jaja. El día anterior nos habíamos visto en el Parque Simón Bolívar, y recuerdo que ni siquiera sabía bien cómo llegar ni cómo moverme por Bogotá, pero todo se solucionó. Yo era muy feliz de verte, de compartir contigo algo nuevo, mi princesa. Ese fin de semana fue especial, porque ahí me di cuenta de algo más grande: que no solo quería que fueras mi novia, sino la mujer con la que quiero casarme algún día. Sentí que todo encajaba, que nuestra conexión era real, y que la vida me había puesto frente a la persona indicada.',
+  emoji: '🍿',
+  photo: 'assets/images/primercine.jpeg'
+},
+{
+  id: 7,
+  date: '2022-05-30',
+  title: 'El día que te pedí que fueras mi novia',
+  shortText: 'Estaba muy nervioso, pero más que nada, emocionado de hacer oficial lo nuestro.',
+  longText: 'Ese fin de semana fue muy especial. Veníamos de vivir días hermosos en Bogotá, y yo ya tenía claro que quería dar el siguiente paso. Sentía que allá no era el lugar, prefería hacerlo en El Carmen, donde me sintiera tranquilo y donde todo tuviera más sentido. Le pedí ayuda a mi hermana, porque nunca había hecho algo así y no sabía cómo hacerlo. Tenía muchas cosas en la cabeza, pero sobre todo, mucha emoción. Viajamos juntos desde Bogotá hasta El Carmen, y yo llevaba todo listo: la cajita, el cartel con la pregunta y un montón de nervios. Cuando llegó el momento, fui a recogerte para una de nuestras salidas normales, aunque esta vez no era una más. Estaba temblando, con la voz entrecortada, pero decidido. Llegué, bajaste, te di la caja y te pedí que la abrieras. Recuerdo tu cara tan feliz y emocionada, y el instante en que me dijiste que sí. Nos dimos un piquito, y ese momento, tus gestos y tu sonrisa, se quedaron grabados en mí para siempre. Subiste a dejar las cosas y salimos a disfrutar nuestra primera salida como novios. Fue un día perfecto, uno que siempre voy a llevar en el corazón.',
+  photo: 'assets/images/dianovios.jpeg'
 }
+
+
 
 ];
